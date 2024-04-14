a3bd69c899414036b46b1135e3f9366a1c38d717
joribus <joridamilaqi@gmail.com>
2024-04-14 20:04:16 +0200

    per farlo girare alla presentazione

--- grafico.py
+++ grafico.py
@@ -35,8 +35,8 @@ for i in range(10):
             r: Individuo = get_r(k, matrix)
             if interaction(k, r):
                 k.copy_trait(r)
-                if check_cells_interaction(matrix):
-                    break
+            if check_cells_interaction(matrix):
+                break
         if plot and (iteration_count == 50000 or iteration_count == 100000):
             plt.imshow(transform_matrix_to_color(transform_matrix(matrix)))
             print("200000")  # colore acceso
@@ -79,7 +79,7 @@ for i in range(20):
 
     numero_regioni = []
 
-    for i in range(5):
+    for i in range(10):
         matrix = create_matrix(L, f, t)
 
         if plot:
@@ -94,8 +94,8 @@ for i in range(20):
             r: Individuo = get_r(k, matrix)
             if interaction(k, r):
                 k.copy_trait(r)
-                if check_cells_interaction(matrix):
-                    break
+            if check_cells_interaction(matrix):
+                break
         if plot and (iteration_count == 50000 or iteration_count == 100000):
             plt.imshow(transform_matrix_to_color(transform_matrix(matrix)))
             print("200000")  # colore acceso
@@ -115,3 +115,218 @@ plt.xlabel('Width of territory')
 plt.ylabel('Average Number of Stable Regions')
 plt.title('Plot of X and Y')
 plt.show()
+
+
+# per LATEX
+
+import os
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from utility import *
+
+plot = True
+numero_regioni = []
+medie = []
+f = 5
+t = 10
+L = 10
+matrix = create_matrix(L, f, t)
+print_matrix(matrix)
+iteration_count = 0  # Initialize iteration count
+
+if plot:
+    plt.imshow(transform_matrix_to_color(transform_matrix(matrix)))  # colore acceso
+    plt.show()
+
+latexMatrix = transform_matrix(matrix)
+latex_code = "\\begin{bmatrix}\n"
+for row in latexMatrix:
+    latex_code += " & ".join(map(str, row)) + " \\\\\n"
+latex_code += "\\end{bmatrix}"
+
+print(latex_code)
+
+if plot:
+    plt.imshow(transform_matrix_to_color(transform_matrix(matrix)))  # colore acceso
+    plt.show()
+
+while True:
+    iteration_count += 1  # Increment iteration count
+    k: Individuo = get_k(matrix)
+    r: Individuo = get_r(k, matrix)
+    if interaction(k, r):
+        k.copy_trait(r)
+    if check_cells_interaction(matrix):
+        break
+    if plot and (iteration_count == 120000 or iteration_count == 200000):
+        plt.imshow(transform_matrix_to_color(transform_matrix(matrix)))
+        print("200000")  # colore acceso
+        plt.show()
+
+numero_regioni.append(number_of_regions(matrix))
+print("num iterazione ", len(numero_regioni))
+
+medie.append(np.mean(numero_regioni))
+print("L ", L)
+
+print(medie)
+
+
+if plot:
+    plt.imshow(transform_matrix_to_color(transform_matrix(matrix)))  # colore acceso
+    plt.show()
+
+latexMatrix = transform_matrix(matrix)
+latex_code = "\\begin{bmatrix}\n"
+for row in latexMatrix:
+    latex_code += " & ".join(map(str, row)) + " \\\\\n"
+latex_code += "\\end{bmatrix}"
+
+print(latex_code)
+
+
+
+# per fare la FIGURA 3
+
+f = 5
+t = 15
+dimensioni = []
+
+iterations_mean = []
+interactions_mean = []
+
+for i in range(10):
+    L = i + 2
+    dimensioni.append(L)
+
+    plot = False
+
+    for j in range(10):
+        matrix = create_matrix(L, f, t)
+        iterations = []
+        interactions = []
+
+        if plot:
+            plt.imshow(transform_matrix_to_color(transform_matrix(matrix)))  # colore acceso
+            plt.show()
+
+        iteration_count = 0  # Initialize iteration count
+        interaction_count = 0
+        while True:
+            iteration_count += 1  # Increment iteration count
+            k: Individuo = get_k(matrix)
+            r: Individuo = get_r(k, matrix)
+            if interaction(k, r):
+                interaction_count += 1
+                k.copy_trait(r)
+            if check_cells_interaction(matrix):
+                break
+        if plot and (iteration_count == 50000 or iteration_count == 100000):
+            plt.imshow(transform_matrix_to_color(transform_matrix(matrix)))
+            print("200000")  # colore acceso
+            plt.show()
+
+        iterations.append(iteration_count)
+        interactions.append(interaction_count)
+
+    print("L ", L)
+    iterations_mean.append(np.mean(iterations))
+    interactions_mean.append(np.mean(interactions))
+
+
+plt.plot(dimensioni, interactions_mean, marker="o")
+plt.grid()
+plt.xlabel('Width of Territory')
+plt.ylabel('Average Number of interactions')
+plt.title('Interactions')
+plt.show()
+
+plt.plot(dimensioni, iterations_mean, marker="o")
+plt.grid()
+plt.xlabel('Width of territory')
+plt.ylabel('Average Number of iterations')
+plt.title('Iterations')
+plt.show()
+
+plt.plot(dimensioni, iterations_mean, marker="o", label="Iterations")
+plt.plot(dimensioni, interactions_mean, marker="o", label="Interactions")
+plt.grid()
+plt.xlabel('Width of territory')
+plt.ylabel('Simulated Time')
+plt.title('Avarage number of iterations and interactions')
+plt.legend(labels=['Iterations', 'Interactions'])
+plt.show()
+
+
+import os
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from utility import *
+
+for w in range(5):
+
+    f = 5
+    t = 15
+    dimensioni = []
+
+    iterations_mean = []
+    interactions_mean = []
+
+    for i in range(9):
+        L = i + 2
+        dimensioni.append(L)
+
+        plot = False
+
+        for j in range(15):
+            matrix = create_matrix(L, f, t)
+            iterations = []
+            interactions = []
+
+            if plot:
+                plt.imshow(transform_matrix_to_color(transform_matrix(matrix)))  # colore acceso
+                plt.show()
+
+            iteration_count = 0  # Initialize iteration count
+            interaction_count = 0
+            while True:
+                iteration_count += 1  # Increment iteration count
+                k: Individuo = get_k(matrix)
+                r: Individuo = get_r(k, matrix)
+                if interaction(k, r):
+                    interaction_count += 1
+                    k.copy_trait(r)
+                if check_cells_interaction(matrix):
+                    break
+            if plot and (iteration_count == 50000 or iteration_count == 100000):
+                plt.imshow(transform_matrix_to_color(transform_matrix(matrix)))
+                print("200000")  # colore acceso
+                plt.show()
+
+            iterations.append(iteration_count)
+            interactions.append(interaction_count)
+
+        print("L ", L)
+        iterations_mean.append(np.mean(iterations))
+        interactions_mean.append(np.mean(interactions))
+
+    plt.plot(dimensioni, iterations_mean, marker="o", label="Iterations")
+    plt.plot(dimensioni, interactions_mean, marker="o", label="Interactions")
+    plt.grid()
+    plt.xlabel('Width of territory')
+    plt.ylabel('Simulated Time')
+    plt.title('Avarage number of iterations and interactions')
+    plt.legend(labels=['Iterations', 'Interactions'])
+    plt.subplots_adjust(left=0.15, right=0.95, top=0.9, bottom=0.1)
+
+    plt.show()
+
+    print("Plot numero: ", w)
+
+
--- main.py
+++ main.py
@@ -5,50 +5,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from utility import *
-
-f = 4
-t = 15
+random.seed(100)
 
 L = 4
+f = 5
+t = 5
+
 matrix = create_matrix(L, f, t)
-print_matrix(matrix)
-
-# Create a plot
-fig, ax = plt.subplots()
-
-# Plot digits of each cell with distance
-for i in range(L):
-    for j in range(L):
-        feature_vector = matrix[i, j].feature
-        print(feature_vector)
-        for idx, digit in enumerate(feature_vector):
-            ax.text(i + 0.2 * idx - 0.3, j, str(digit), ha='center', va='center', fontsize=10)
-            print(digit)
-# Add grid lines
-for i in range(L+1):
-    ax.axvline(x=i-0.5, color='gray', linestyle='-')
-    ax.axhline(y=i-0.5, color='gray', linestyle='-')
-
-# Set x and y limits
-ax.set_xlim([-0.5, L-0.5])
-ax.set_ylim([-0.5, L-0.5])
-ax.set_aspect('equal', 'box')
-
-# Set labels
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-
-# Remove ticks
-ax.set_xticks([])
-ax.set_yticks([])
-
-# Show the plot
-plt.grid(False)
+plt.imshow(transform_matrix_to_color(transform_matrix(matrix)))  # colore acceso
 plt.show()
-"""
-if plot:
-    plt.imshow(transform_matrix_to_color(transform_matrix(matrix)))  # colore acceso
-    plt.show()
 
 iteration_count = 0  # Initialize iteration count
 
@@ -58,20 +23,13 @@ while True:
     r: Individuo = get_r(k, matrix)
     if interaction(k, r):
         k.copy_trait(r)
-        if check_cells_interaction(matrix):
-            break
-if plot and (iteration_count == 50000 or iteration_count == 100000):
-    plt.imshow(transform_matrix_to_color(transform_matrix(matrix)))
-    print("200000")  # colore acceso
-    plt.show()
+    if check_cells_interaction(matrix):
+        break
 
-numero_regioni.append(number_of_regions(matrix))
-print("num iterazione ", len(numero_regioni))
+    if iteration_count % 100 == 0:
+        plt.imshow(transform_matrix_to_color(transform_matrix(matrix)))  # colore acceso
+        plt.show()
 
-medie.append(np.mean(numero_regioni))
-print("L ", L)
-
-print(medie)
-print(dimensioni)
-
-"""
+plt.imshow(transform_matrix_to_color(transform_matrix(matrix)))  # colore acceso
+plt.show()
+print(iteration_count)
